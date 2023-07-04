209b53e4a1fcaf59255dd127781c53ac9e22da44
Aibek <ai.bek@mail.ru>
2023-07-04 12:22:45 +0600

    feat: add all crud methods

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
-import { error } from 'console'
 import dotenv from 'dotenv'
 import http from 'http'
-import { Users } from './db/users.js'
+import { handleRequest } from './controls/controls.js'
 dotenv.config()
 
-const server = http.createServer((req, res)=> {
-  res.setHeader('Content-type', 'application/json')
-  if(req.url === '/users' && req.method === 'GET') {
-    req.url
-    const users = Users.getUsers()
-    res.statusCode = 200
-    res.write(JSON.stringify(users))
-    res.end()
-  }
-  })
+const server = http.createServer(handleRequest)
 const port = Number(process.env.PORT)
-server.listen(port, () => `listen port ${port}`).on('error', (err) => console.log(err))
+
+server.listen(port, () => console.log( `server running on port ${port}`)).on('error', (err) => console.log(err))
+
+
 
